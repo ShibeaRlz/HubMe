5e8b8567dd89815c16f45a80e3b5bb6a8d8367cf
MAOZ <maooz4426@gmail.com>
2024-11-03 07:05:12 +0900

    stylelintの設定を追加

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+"use client";
+import { Community } from "@/domain/community";
+import { statusAtom } from "@/domain/general";
+import { userAtom } from "@/domain/user";
+import { type ClassValue, clsx } from "clsx";
+import { useAtom } from "jotai/index";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
