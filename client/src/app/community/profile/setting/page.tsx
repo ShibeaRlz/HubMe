48cd5a2c58717b2d0a85b5837b169b8460f9c5a0
MAOZ <maooz4426@gmail.com>
2024-12-12 01:49:42 +0900

    user/homeにいけないバグを修正

--- client/src/app/community/profile/setting/page.tsx
+++ client/src/app/community/profile/setting/page.tsx
@@ -1,10 +1,11 @@
 import { ProfileSetting } from "@/features/profile/components/ProfileSetting";
+import style from "./style.module.scss";
 
 const ProfileSettingPage = () => {
   return (
-    <>
+    <div className={style.container}>
       <ProfileSetting type="community" />
-    </>
+    </div>
   );
 };
 
